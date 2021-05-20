9c3092a758ec710d226aa96e3779ef2c7d7510bc
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-05-20 18:38:43 +0800

    About Us

---
+++ us.php
@@ -0,0 +1,40 @@
+<?php 
+include 'koneksi.php';
+include 'head.php'; 
+include 'slide-hal.php';
+?> 
+
+<body>
+
+<?php include 'nav.php'; ?>
+
+<div class="container" style="margin-top:30px">
+
+  <div class="row">
+    <div class="col-sm-12">
+      <center><img src="foto/penting/logob.jpg" width="300" height="300" bg-color="black"></center>
+      </br>
+      <p>why is the name lamper megibung? because megibung has become our tradition here especially the island of Lombok is a typical tradition passed down from generation to generation. this tradition is still very thick in our village, namely lamper village and therefore we intend to create a website that is characterized by our village and lampermegibung.com was formed. we hope that with this website we can introduce our culture and traditions (lamper village), and also build a tourism that is related to traveling, touring, diving, snorkeling, etc. and of course this website also provides a variety of choices of tourist attractions that have been divided into 4 categories namely mountains, waterfalls, forests, and of course the beautiful beaches of this island.</p> 
+      <p>we also have a guide information that has been listed on this web and then also has an agent who has collaborated with this website to provide facilities that you might want to try if you visit our village and of course Lombok Island, have information about what events are in the village lamper, among others, is a religious ceremony that is often held in our village which is called 'piodalan at tample', and our daily lives are in lamper village. so hurry up and register yourself at registration and explore the island of Lombok with us. thank you</p>
+      </br>
+      <p>
+        <b>rent and payment</b></br>
+        - First, click the registration menu in the Nav bar</br>
+        - Second, fill in the data in the form</br>
+        - Third, confirmation will enter in your email</br>
+        - Fourt, pay the reservasion to BRI BANK INDONESIA. Rek Number : </br>
+      </p>
+      </br>
+      <b><p>Location :</p></b>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d986.0954533763548!2d116.16522288678588!3d-8.655188896672321!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfcddc2c8349f172d!2sLamper+Megibung!5e0!3m2!1sid!2sid!4v1564994830004!5m2!1sid!2sid" width="100%" height="500" frameborder="1" style="border:0" allowfullscreen></iframe>
+
+      
+
+    </div>
+  </div>
+
+
+  </div>
+</div>
+
+<?php include 'footer.php'; ?>
